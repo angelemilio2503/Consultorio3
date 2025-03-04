5c06d497db60043be4e6835e246425e6acc50a5d
angelemilio2503 <angelemilio2503@gmail.com>
2025-03-03 19:35:24 -0600

    Mover index.html y corregir configuración de Vite

--- frontend/sistema-consultorio/dist/components/Button/CustomButton.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const material_1 = require("@mui/material");
-const CustomButton = ({ label, onClick, variant = "contained", color = "primary", disabled = false, fullWidth = false, }) => {
-    return ((0, jsx_runtime_1.jsx)(material_1.Button, { variant: variant, color: color, onClick: onClick, disabled: disabled, fullWidth: fullWidth, children: label }));
-};
-exports.default = CustomButton;
--- frontend/sistema-consultorio/vite.config.ts
+++ frontend/sistema-consultorio/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist', // Carpeta de salida de la build
+  },
   server: {
     watch: {
       usePolling: true
