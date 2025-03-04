ebbf8b9d7504babed9e51855240197e8c751e759
angelemilio2503 <angelemilio2503@gmail.com>
2025-03-03 19:47:12 -0600

    Todo a la normalidad

--- frontend/sistema-consultorio/vite.config.ts
+++ frontend/sistema-consultorio/vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: '.',
-  build: {
-    outDir: 'dist', // Carpeta de salida de la build
-  },
   server: {
     watch: {
       usePolling: true
